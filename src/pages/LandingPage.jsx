f3927007e77e91e0e29827c150c3f5266ebc5781
David Emmanuel <eclefzy@gmail.com>
2025-08-29 15:49:03 +0100

    worked on some sections of the landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,12 +7,12 @@ const LandingPage = () => {
 
       <div className='container-fluid' style={{ padding: '2rem', backgroundColor: '#198754', marginBottom: '-2rem' }}>
         <div className='container' style={{ color: 'white', textAlign: 'center', padding: '2rem' }}>
-          <h1>ready to transform agricultural products and orders?</h1>
-          <p>Join thousands of farmers and customers who are already part of our growing community</p>
+          <h1 style={{ fontSize: '2.5rem', fontWeight: 'bold', textTransform: 'uppercase' }}>ready to transform agricultural orders?</h1>
+          <p style={{ display: 'inline-block', margin: '1rem 0', fontSize: '1.0rem' }}>Join thousands of farmers and customers who are already part of our growing community</p>
           <div className='button-group' style={{ display: 'flex', justifyContent: 'center', gap: '1rem' }}>
             <button style={{ backgroundColor: 'white', color: '#198754', border: 'none', padding: '0.5rem 1rem', borderRadius: '5px' }}>Get Started</button>
             <button style={{ backgroundColor: 'white', color: '#198754', border: 'none', padding: '0.5rem 1rem', borderRadius: '5px' }}>Learn More</button>
-            <button style={{ backgroundColor: 'white', color: '#198754', border: 'none', padding: '0.5rem 1rem', borderRadius: '5px' }}>Become a Partner</button>
+            <button style={{ backgroundColor: 'white', color: 'black', border: 'none', padding: '0.5rem 1rem', borderRadius: '5px' }}><span><i className="bi bi-people" style={{ color: '#212529', marginRight: '0.5rem' }}></i></span>Become a Partner</button>
           </div>
         </div>
       </div>
